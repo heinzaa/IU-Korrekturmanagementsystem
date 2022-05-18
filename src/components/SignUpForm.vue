3aca3ca59211407191fe237b5bd303064206497c
Marc Heintz <heintz.marc@web.de>
2022-05-18 20:01:38 +0200

    login ready

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+      <input type="text" required placeholder="display name" v-model="displyName" />
+      <input type="email" required placeholder="email" v-model="email" />
+      <input type="password" required placeholder="password" v-model="password" />
+      <div class="error"> {{error}}</div>
+      <button id="login">Sign up </button>
+  </form>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import useSignup from '../composables/useSignup'
+export default {
+   setup(props, context){ 
+       const { error, signup } = useSignup()
+       // refs
+       const displyName = ref('');
+       const email = ref('');
+       const password = ref('');
+      const handleSubmit = async() => {
+        await signup(email.value, password.value, displyName.value);
+         if(!error.value){
+              context.emit('SignUp');
+          }
+      }
+       return {displyName, email, password, handleSubmit, error}
+   }
+}
+</script>
+
+<style>
+ #login {
+        cursor: pointer;
+    }
+</style>
