5432a48cf4533d9c6a6fea9385c3cd538108b096
Marc Heintz <heintz.marc@web.de>
2022-05-30 23:25:08 +0200

    Registrierung abgeschlossen und deployment eingereichtet

--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ import App from './App.vue'
 import router from './router'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(BootstrapIconsPlugin)
+    .mount('#app')
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,6 +4,7 @@
         <div class="dashboard container">
             <h1>Willkommen im Dashboard</h1>
         </div>
+         <b-icon-arrow-up></b-icon-arrow-up>
         <TemplateFooter />
     </div>
 </template>
