0753be5eb90f6912d2973f7f7ed137a03c434d91
Marc Heintz <heintz.marc@web.de>
2022-05-31 23:08:15 +0200

    Pending Zustand wenn ein Asyncronous call ausgeführt wird

--- src/composables/useLogin.js
+++ src/composables/useLogin.js
@@ -2,26 +2,28 @@ import { ref } from 'vue'
 import { projectAuth } from '../firebase/config'
 
 const error = ref(null);
+const isPending = ref(false);
 
 const login = async (email, password) => {
 
     error.value = null;
+    isPending.value = true;
 
     try {
         const res = await projectAuth.signInWithEmailAndPassword(email, password);
         error.value = null;
-        console.log(res);
-        console.log("Marc");
+        isPending.value = false;
         return res;
     } catch (err) {
         console.log(err.value);
         error.value = 'Incorrect login credentials';
+        isPending.value = false;
     }
 
 }
 
 const useLogin = () => {
-    return { error, login }
+    return { error, login, isPending }
 }
 
 export default useLogin
