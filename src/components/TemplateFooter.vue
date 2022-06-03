c78926a157a7c6c0583a22e739bc0564346fa0ba
Reini <reini@localhost>
2022-06-03 19:45:22 +0200

    Footer: Rechtschreibfehler korrigiert

--- src/components/TemplateFooter.vue
+++ src/components/TemplateFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="footer">
         <div class="container">
-            <div style="font-size:0.7rem;">Dies ist ein privates Studentenprojekt der IU Internationelen&nbsp;Hochschule</div>
+            <div style="font-size:0.7rem;">Dies ist ein privates Studentenprojekt der IU Internationalen&nbsp;Hochschule</div>
             <img src="/iu-logo.svg" alt="Logo IU" />
         </div>
     </div>
