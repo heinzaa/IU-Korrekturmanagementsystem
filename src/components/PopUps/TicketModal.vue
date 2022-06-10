0f049b68719c56407fb3774e43ade481d34dd476
Marc Heintz <heintz.marc@web.de>
2022-06-11 00:12:50 +0200

    PopUp Modal

---
+++ src/components/PopUps/TicketModal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+
+      <div >
+        <slot />
+        <button type="button" @click="$emit('close')">Close</button>
+      </div>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>
