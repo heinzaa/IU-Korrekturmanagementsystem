efa68d728ffc10b76118e3767b47bc6bf3952bdc
Reini <reini@localhost>
2022-06-15 00:48:13 +0200

    Ticketdetails: Template gestyled/organisiert, Infos hinzugefügt, Bild-Vorschau verkleinert und verlinkt

--- src/views/TicketDetails.vue
+++ src/views/TicketDetails.vue
@@ -1,81 +1,85 @@
 <template>
-<div>
-    <div v-if="error" class="error" > {{ error }} </div>
+    <div id="template">
+        <TemplateHeader />
 
-    <div v-if="document">
-        
-        
-       
+        <div class="container content-small">
+            <h1>Ticketinformationen</h1>
+            <hr />
 
-    <div style="background:#000; padding:1em; margin:2em 0; color:#fff; text-align:center;">
-                - AB HIER TICKET FORMULAR FÜR TUTOR -
+            <div v-if="error" class="error" style="margin:1em 0; text-align:center;"> {{ error }} </div>
+
+            <div v-if="document">
+                <form action="">
+                    <div class="mb-4">
+                        <label class="view">Titel (Überschrift):</label>
+                        <div>{{ document.title }}</div>
+                    </div>
+                    <div class="mb-4">
+                        <label class="view">Kurs:</label>
+                        <div>{{document.courseInformation.course}} </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-4 col-md-6">
+                            <label class="view">Kategorie, Art der Meldung:</label>
+                            <div>{{document.category}}</div>
+                        </div>
+                        <div class="mb-4 col-md-6">
+                            <label class="view">Priorität:</label>
+                            <div>{{document.priority}}</div>
+                        </div>
+                    </div>
+                    <div class="mb-4">
+                        <p><label class="view">Betroffenen Medien:</label></p>
+                        <ul>
+                            <li>
+                                <label>Learn App/IU Reader:</label>
+                                <div>Blabla hier steht irgend was zur Position</div>
+                            </li>
+                            <li>
+                                <label>Skript:</label>
+                                <div>Blabla jhdfskaof jhsdiof ji</div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mb-4">
+                        <label class="view">Beschreibe deine Ticketmeldung:</label>
+                        <div>{{document.description}}</div>
+                    </div>
+                    <div class="mb-4">
+                        <label class="view">Dateien:</label>
+                        <div><a :href="document.fileUrl" target="_blank"><img :src="document.fileUrl" class="img-preview" /></a></div>
+                    </div>
+
+                    <hr>
+                    <h3>Bearbeitung durch Tutor</h3>
+
+                    <div class="mb-4">
+                        <label for="feedbackComment">Tutor-Feedback:</label>
+                        <textarea class="form-control" name="feedbackComment" id="feedbackComment" style="min-height:100px;"></textarea>
+                    </div>
+                    <div class="mt-4">
+                        Status ändern:
+                    </div>
+                    <div class="mt-1">
+                        <button type="button" class="btn btn-warning">In Arbeit</button> &nbsp; 
+                        <button type="button" class="btn btn-success">Erledigt</button> &nbsp; 
+                        <button type="button" class="btn btn-info">Abgelehnt</button>
+                    </div>
+                    <hr>
+                </form>
             </div>
 
-            <form action="">
-                <div class="mb-4">
-                    <label class="view">Titel (Überschrift):</label>
-                    <div>{{ document.title }}</div>
-                </div>
-                <div class="mb-4">
-                    <label class="view">Kurs:</label>
-                    <div>{{document.courseInformation.course}} </div>
-                </div>
-                <div class="row">
-                    <div class="mb-4 col-md-6">
-                        <label class="view">Kategorie, Art der Meldung:</label>
-                        <div>{{document.categoryText}}</div>
-                    </div>
-                    <div class="mb-4 col-md-6">
-                        <label class="view">Priorität:</label>
-                        <div>{{document.priority}}</div>
-                    </div>
-                </div>
-                <div class="mb-4">
-                    <p><label class="view">Betroffenen Medien:</label></p>
-                    <ul>
-                        <li>
-                            <label>Learn App/IU Reader:</label>
-                            <div>Blabla hier steht irgend was zur Position</div>
-                        </li>
-                        <li>
-                            <label>Skript:</label>
-                            <div>Blabla jhdfskaof jhsdiof ji</div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="mb-4">
-                    <label class="view">Beschreibe deine Ticketmeldung:</label>
-                    {{document.description}}
-                </div>
-                <div class="mb-4">
-                    <label class="view">Dateien:</label>
-                    <img :src="document.fileUrl" />
-                </div>
-
-                <hr>
-                <h3>Bearbeitung durch Tutor</h3>
-
-                <div class="mb-4">
-                    <label for="feedbackComment">Tutor-Feedback:</label>
-                    <textarea class="form-control" name="feedbackComment" id="feedbackComment" style="min-height:100px;"></textarea>
-                </div>
-                <div class="mt-4">
-                    Status ändern:
-                </div>
-                <div class="mt-1">
-                    <button type="button" class="btn btn-warning">In Arbeit</button> &nbsp; 
-                    <button type="button" class="btn btn-success">Erledigt</button> &nbsp; 
-                    <button type="button" class="btn btn-info">Abgelehnt</button>
-                </div>
-                <hr>
-            </form>
-            </div>
+        </div>
+        <TemplateFooter />
      </div>
 </template>
 
 <script>
+import TemplateHeader from "../components/TemplateHeader.vue";
+import TemplateFooter from "../components/TemplateFooter.vue";
 import getDocument from '../composables/getDocument'
 export default {
+    components: { TemplateHeader, TemplateFooter },
     props: ['id'],
 
     setup(props){
@@ -88,5 +92,10 @@ export default {
 </script>
 
 <style>
-
+label.view {
+    color:#999;
+}
+.img-preview {
+    max-width:200px; height:auto;
+}
 </style>
