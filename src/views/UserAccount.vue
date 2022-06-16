68eb6c6f7e1f835559eb95852480676c0f2636d3
Reini <reini@localhost>
2022-06-17 00:33:56 +0200

    Kontolöschung: Warnhinweis und Bestätigung abfragen

--- src/views/UserAccount.vue
+++ src/views/UserAccount.vue
@@ -51,10 +51,20 @@
             <p>&nbsp;</p>
 
             <!-- Konto-Löschung -->
-            <p style="text-align:center;">
-                <button type="button" class="btn btn-outline-secondary" @click="deleteUserAccount">Benutzerkonto löschen?</button>
-            </p>
+            <div style="text-align:center;">
+                <button id="buttonForConfirm" type="button" class="btn btn-outline-secondary" onclick="document.getElementById('confirmArea').style.display='block'; this.style.display='none';">Benutzerkonto löschen?</button>
+                <div id="confirmArea" style="display:none;">
+                    <h3>Konto wirklich löschen?</h3>
+                    <p>Mit der Löschung deines Benutzerkontos werden deine Kontodaten sowie eingereichte Tickets gelöscht. Der Vorgang kann nicht rückgängig gemacht werden.
+                        Anschließend ist eine Neuregistrierung jederzeit wieder möglich.
+                    </p>
+                    <button id="deleteUser" type="button" class="btn btn-danger" @click="deleteUserAccount">Löschung durchführen</button>
+                    <div class="mb-3"></div>
+                    <button type="button" class="btn btn-outline-secondary" onclick="document.getElementById('buttonForConfirm').style.display='inline-block'; this.parentNode.style.display='none';">Abbrechen</button>
+                </div>
+            </div>
             <div v-if="deleteUserError" class="error">{{deletionFailed}}</div>
+            <p>&nbsp;</p>
         </div>
         <TemplateFooter />
     </div>
@@ -64,83 +74,71 @@
 import TemplateHeader from "../components/TemplateHeader.vue";
 import TemplateFooter from "../components/TemplateFooter.vue";
 import { useRouter } from "vue-router";
-import getUser from '../composables/getUser'
+import getUser from "../composables/getUser";
 import { ref } from "@vue/reactivity";
-import useDeleteUser from '../composables/useDeleteUser';
+import useDeleteUser from "../composables/useDeleteUser";
 
-import useUpdatePassword from '../composables/useUpdatePassword';
+import useUpdatePassword from "../composables/useUpdatePassword";
 
 export default {
     components: {
         TemplateHeader,
-        TemplateFooter
+        TemplateFooter,
     },
-    mounted(){
-        document.querySelector('#mainmenu li a').classList.remove('active');
-        document.getElementById('navbarDropdownAccount').classList.add('active');
+    mounted() {
+        document.querySelector("#mainmenu li a").classList.remove("active");
+        document
+            .getElementById("navbarDropdownAccount")
+            .classList.add("active");
     },
     setup() {
-        
         const router = useRouter();
         const { error, updatePassword } = useUpdatePassword();
         const { user } = getUser();
-        const passwordOld = ref('');
-        const passwordNew = ref('');
-        const passwordNewControl = ref('');
+        const passwordOld = ref("");
+        const passwordNew = ref("");
+        const passwordNewControl = ref("");
         const passwordNewEqualsOld = ref(false);
-        const passwordNewEqualsControl = ref(false);   
-        const isPending = ref('');        
+        const passwordNewEqualsControl = ref(false);
+        const isPending = ref("");
 
-        const { deleteUserError, deleteUserHehe, isPendingDeleteUser } =  useDeleteUser();
+        const { deleteUserError, deleteUserHehe, isPendingDeleteUser } =
+            useDeleteUser();
 
-        
-
-        
-       
-
-        
-        
-
-        
-
-        
-
-       const changePassword = async() => {     
-            
+        const changePassword = async () => {
             isPending.value = true;
 
-           if(passwordOld.value == passwordNew.value ){
-               passwordNewEqualsOld.value = true;
-               passwordNewEqualsControl.value = false;
-               
-           }
-           if(passwordNew.value != passwordNewControl.value){
-               passwordNewEqualsControl.value = true
-               passwordNewEqualsOld.value = false;
-               
-           } 
+            if (passwordOld.value == passwordNew.value) {
+                passwordNewEqualsOld.value = true;
+                passwordNewEqualsControl.value = false;
+            }
+            if (passwordNew.value != passwordNewControl.value) {
+                passwordNewEqualsControl.value = true;
+                passwordNewEqualsOld.value = false;
+            }
 
-           await updatePassword(passwordNew.value); 
+            await updatePassword(passwordNew.value);
 
             isPending.value = false;
-            
+        };
 
-       }
+        const deleteUserAccount = async () => {
+            await deleteUserHehe();
+            router.push('/');
+        };
 
-        const deleteUserAccount = async()  => {
-           
-          await deleteUserHehe();
-          router.push('/');
-
-        }
-
-
-
-       return { user, passwordOld, passwordNew, passwordNewControl, passwordNewEqualsOld, passwordNewEqualsControl, isPending, changePassword, deleteUserAccount , deleteUserError}
-
-       
-
-    
+        return {
+            user,
+            passwordOld,
+            passwordNew,
+            passwordNewControl,
+            passwordNewEqualsOld,
+            passwordNewEqualsControl,
+            isPending,
+            changePassword,
+            deleteUserAccount,
+            deleteUserError,
+        };
     },
 };
 </script>
