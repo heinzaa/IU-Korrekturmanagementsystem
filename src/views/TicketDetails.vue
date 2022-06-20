7e71a5e765bf6c488ed648e8709e7f4222a35d03
Reini <reini@localhost>
2022-06-20 23:24:05 +0200

    Ticketdetails: Medienauswahl korrekt ergänzt, Erstellungsdatum + Autor ergänzt, Hinweis für Tutor zur Nachbearbeitung

--- src/views/TicketDetails.vue
+++ src/views/TicketDetails.vue
@@ -31,18 +31,46 @@
                     <div class="mb-4">
                         <p><label class="view">Betroffenen Medien:</label></p>
                         <ul>
-                            <li>
+                            <li v-if="document.LearnApp_Fehler[0]">
                                 <label>Learn App/IU Reader:</label>
-                                <div>Blabla hier steht irgend was zur Position</div>
+                                <div>{{ document.LearnApp_Fehler[1] }}</div>
                             </li>
-                            <li>
-                                <label>Skript:</label>
-                                <div>Blabla jhdfskaof jhsdiof ji</div>
+                            <li v-if="document.PDF_Skript_Fehler[0]">
+                                <label>Skript <span class="form-text">(PDF)</span></label>
+                                <div>{{ document.PDF_Skript_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Druckskript_Fehler[0]">
+                                <label>Skript <span class="form-text">(Druck)</span></label>
+                                <div>{{ document.Druckskript_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Video_Fehler[0]">
+                                <label>Video <span class="form-text">(Vodcast, Tutorium)</span></label>
+                                <div>{{ document.Video_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Audio_Fehler[0]">
+                                <label>Audio <span class="form-text">(Podcast)</span></label>
+                                <div>{{ document.Audio_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.OnlineTest_Fehler[0]">
+                                <label>Online-Test <span class="form-text">(MyCampus)</span></label>
+                                <div>{{ document.OnlineTest_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Repetitorium_Fehler[0]">
+                                <label>Repetitorium</label>
+                                <div>{{ document.Repetitorium_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Musterklausur_Fehler[0]">
+                                <label>Musterklausur</label>
+                                <div>{{ document.Musterklausur_Fehler[1] }}</div>
+                            </li>
+                            <li v-if="document.Sonstige_Fehler[0]">
+                                <label>Sonstiges</label>
+                                <div>{{ document.Sonstige_Fehler[1] }}</div>
                             </li>
                         </ul>
                     </div>
                     <div class="mb-4">
-                        <label class="view">Beschreibe deine Ticketmeldung:</label>
+                        <label class="view">Beschreibung:</label>
                         <div>{{document.description}}</div>
                     </div>
                     <div class="mb-4">
@@ -55,9 +83,24 @@
                         </div>
                         <div v-else>Keine Datei vorhanden.</div>
                     </div>
+                    <div class="row">
+                        <div class="mb-4 col-md-6">
+                            <label class="view">Erstellung:</label>
+                            <div>
+                                {{String(document.createdAt.toDate().getDate()).padStart(2,'0')}}.{{String(document.createdAt.toDate().getMonth()+1).padStart(2,'0')}}.{{document.createdAt.toDate().getFullYear()}}
+                                - {{String(document.createdAt.toDate().getHours()).padStart(2,'0')}}:{{String(document.createdAt.toDate().getMinutes()).padStart(2,'0')}} Uhr
+                            </div>
+                        </div>
+                        <div class="mb-4 col-md-6">
+                            <label class="view">Autor:</label>
+                            <div>{{document.authorName}}</div>
+                        </div>
+                    </div>
 
                     <hr>
                     <h3>Bearbeitung durch Tutor</h3>
+
+                    <p>@ToDo: Diese Bearbeitung nur dann zulassen, wenn Status = "Offen" oder "In Arbeit". Ansonsten nur anzeigen.</p>
 
                     <div class="mb-4">
                         <label for="feedbackComment">Tutor-Feedback:</label>
@@ -70,6 +113,10 @@
                         <button type="button" class="btn btn-warning">In Arbeit</button> &nbsp; 
                         <button type="button" class="btn btn-success">Erledigt</button> &nbsp; 
                         <button type="button" class="btn btn-info">Abgelehnt</button>
+                    </div>
+                    <div class="mt-2">
+                        <small>Hinweis: Bei Änderung des Status wird der Ersteller per E-Mail inkl. angegebenem Feedback informiert.
+                        Bei Änderung des Status auf "Erledigt" oder "Abgelehnt" wird das Ticket zudem geschlossen uns ist nicht mehr editierbar.</small>
                     </div>
                     <hr>
                 </form>
