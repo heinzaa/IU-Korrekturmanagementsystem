f84c3c4a6ff9b556bfbf7312fd912748bcbc61b6
Reini <reini@localhost>
2022-06-20 23:02:15 +0200

    Ticketübersicht: Priorität hinzugefügt (leicht farblich), Spalte "Datum" verkleinert mit fester Breite

--- src/components/DashboardTutor.vue
+++ src/components/DashboardTutor.vue
@@ -29,10 +29,10 @@
                 <table class="table table-hover table-tickets">
                     <thead>
                         <tr>
-                            <th scope="col">Datum</th>
+                            <th scope="col" style="width:130px;">Datum</th>
+                            <th scope="col" style="width:130px;">Priorität</th>
                             <th scope="col">Titel</th>
                             <th scope="col">Kurs</th>
-                            <th scope="col">Autor</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -43,10 +43,21 @@
                                 <strong v-if="ticket.status == 'Abgelehnt'" class="ticket-status rounded-pill bg-info" title="Abgelehnt"></strong>
                                 <strong v-if="ticket.status == 'In Arbeit'" class="ticket-status rounded-pill bg-warning" title="In Arbeit"></strong>
                                 {{String(ticket.createdAt.toDate().getDate()).padStart(2,'0')}}.{{String(ticket.createdAt.toDate().getMonth()+1).padStart(2,'0')}}.{{ticket.createdAt.toDate().getFullYear()}}
+
+                                <span class="only-mobile">
+                                    - 
+                                    <b v-if="ticket.priority == 'Niedrig'" class="text-black-50">Niedrig</b>
+                                    <b v-if="ticket.priority == 'Mittel' " class="">Mittel</b>
+                                    <b v-if="ticket.priority == 'Hoch'"  class="text-danger">Hoch</b>
+                                </span>
+                            </td>
+                            <td class="ticket-priority" data-title="Priorität">
+                                <span v-if="ticket.priority == 'Niedrig'" class="text-black-50">Niedrig</span>
+                                <span v-if="ticket.priority == 'Mittel' " class="">Mittel</span>
+                                <span v-if="ticket.priority == 'Hoch'"  class="text-danger">Hoch</span>
                             </td>
                             <td class="ticket-title" data-title="Titel">{{ticket.title}}</td>
                             <td class="ticket-course" data-title="Kurs">{{ticket.courseInformation.course}}</td>
-                            <td class="ticket-author" data-title="Autor">{{ticket.authorName}}</td>
                         </tr>
                     </tbody>
                 </table>
