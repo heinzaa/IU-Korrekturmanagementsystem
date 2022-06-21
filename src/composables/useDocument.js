d09cc5586b7b11df43562d6da0dee766c96db7ab
Marc Heintz <heintz.marc@web.de>
2022-06-21 16:39:35 +0200

    Composable um Updates an Tickets vorzunehmen

---
+++ src/composables/useDocument.js
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+
+
+const useDocument = (collection, id) => {
+    const error = ref(null)
+    const isPending = ref(false);
+
+    let documentRef = projectFirestore.collection(collection).doc(id);
+
+    const updateDoc = async (updates) =>{
+        isPending.value = true;
+        error.value = null;
+
+        try{
+            const res = await documentRef.update(updates)
+            isPending.value = false;
+            return res;
+        } catch(err){
+            console.log(err.message)
+            isPending.value = false;
+            error.value = 'Ticket konnte nicht aktualisiert werden.'
+        }
+    }
+    return {error, isPending, updateDoc}
+}
+
+export default useDocument
