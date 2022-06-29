29e95b95c3add87d219da57c6df708ca9de8828d
Reini <reini@localhost>
2022-06-29 22:27:33 +0200

    Bugfix Consolenfehler

--- src/views/TicketDetails.vue
+++ src/views/TicketDetails.vue
@@ -4,12 +4,10 @@
 
         <div class="container content-small">
             <h1>Ticketinformationen</h1>
-            <div v-if="document.modifiedAt">
-                <p v-if="document.status == 'Erledigt' || document.status == 'Abgelehnt'">
-                    Ticket geschlossen am {{String(document.modifiedAt.toDate().getDate()).padStart(2,'0')}}.{{String(document.modifiedAt.toDate().getMonth()+1).padStart(2,'0')}}.{{document.modifiedAt.toDate().getFullYear()}}
-                    - {{String(document.modifiedAt.toDate().getHours()).padStart(2,'0')}}:{{String(document.modifiedAt.toDate().getMinutes()).padStart(2,'0')}} Uhr
-                </p>
-            </div>
+            <p v-if="document.status == 'Erledigt' || document.status == 'Abgelehnt'">
+                Ticket geschlossen am {{String(document.modifiedAt.toDate().getDate()).padStart(2,'0')}}.{{String(document.modifiedAt.toDate().getMonth()+1).padStart(2,'0')}}.{{document.modifiedAt.toDate().getFullYear()}}
+                - {{String(document.modifiedAt.toDate().getHours()).padStart(2,'0')}}:{{String(document.modifiedAt.toDate().getMinutes()).padStart(2,'0')}} Uhr
+            </p>
             <hr />
 
             <div v-if="error" class="error" style="margin:1em 0; text-align:center;"> {{ error }} </div>
